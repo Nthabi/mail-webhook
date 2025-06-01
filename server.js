16b54e15a7873cef58fb5d7e072c110a9f467cea
Nthabi <nthabimashiane@gmail.com>
2025-06-01 06:25:19 +0200

    update port

--- server.js
+++ server.js
@@ -32,5 +32,11 @@ app.get('/health', (req, res) => {
     res.json("App is healthy");
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 
 app.listen(3000, () => console.log("MedReminder server running on port 3000"));

153d4f1cf9eda5b5484cbf4d90b85cb4ced7af84
Nthabi <nthabimashiane@gmail.com>
2025-06-01 10:34:57 +0200

    add confirmation mail to test outbound email

--- mailHandler.js
+++ mailHandler.js
@@ -1,10 +1,15 @@
 const db = require('./db');
 const { parseReminder } = require('./parseUtils');
 const { scheduleReminder } = require('./scheduler');
+const postmark = require('postmark');
+const client = new postmark.ServerClient(process.env.POSTMARK_API_TOKEN);
 
 async function handleIncomingEmail(emailData) {
     const sender = emailData.FromFull.Email;
     const body = emailData.TextBody || emailData.HtmlBody;
+    const subject = emailData.Subject || 'Your message';
+    
+    console.log('Received inbound email from:', sender);
 
     const { task, time, frequency } = parseReminder(body);
 
@@ -15,6 +20,13 @@ async function handleIncomingEmail(emailData) {
     stmt.run(sender, task, time, frequency);
 
     scheduleReminder({ email: sender, task, time, frequency });
+
+    await client.sendEmail({
+        From: process.env.SENDER_EMAIL,
+        To: sender,
+        Subject: 'We received your message!',
+        TextBody: `You said: "${body}"\n\nWe'll remind you to: ${task} at ${time} (${frequency}).`
+    });
 }
 
 module.exports = { handleIncomingEmail };
